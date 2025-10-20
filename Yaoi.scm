7204c0042965ceaac68e3daf1b24e1b885e810c9
Joseph Osako <josephosako@gmail.com>
2025-10-20 11:59:24 -0400

    Added tests for make-dispatchable-class and fixed several detected errors.

--- Yaoi.scm
+++ Yaoi.scm
@@ -8,7 +8,9 @@
    (rnrs base (6))
    (rnrs exceptions (6))
    (rnrs conditions (6))
-   (Yaoi conditions))
+   (srfi 28)
+   (Yaoi conditions)
+   (Scheme-Exception-Utils error-reporting))
  
  (define-syntax make-dispatchable-class
    (syntax-rules
@@ -16,11 +18,15 @@
     ((_ (?param-0 ...)
         (methods (?method-name-0 ?method-code-0) ...))
      (lambda (?param-0 ...)
-             (lambda (dispatch . params)
-                     (case dispatch
-                       ((?method-name-0)
-                        ((lambda ( . x) ?method-code-0) params)) ...
-                       (else
-                        ((add-error-reporting
-                          'dispatchable-class
-               				       make-invalid-message-violation))))))))))
+             (lambda
+              (dispatch . params)
+              (let ((report-error
+                     (add-error-reporting
+                      'dispatchable-class
+             				     make-invalid-message-violation)))
+                (case dispatch
+                  ((?method-name-0)
+                   ((lambda ( . x) ?method-code-0) params)) ...
+                  (else (report-error
+                          (format "~a: ~a is not a method name~%"
+                                  'dispatchable-class dispatch)))))))))))
--- Yaoi/tests/test-make-dispatchable-class.scm
+++ Yaoi/tests/test-make-dispatchable-class.scm
@@ -15,9 +15,30 @@
 
 (test-with-runner
  runner
- (test-group "test basic class creation"
-             (let ((cls-0 (make-dispatchable-class
-                           (foo bar baz)
-                           (methods (quux (display foo))
-                                    (zark (display baz))))))
-                (test-assert (procedure? cls-0)))))
+ (test-group
+  "test class creation"
+  (let ((cls-0 (make-dispatchable-class
+                (foo bar baz)
+                (methods (quux foo)
+                         (zark (begin
+                                (display baz)
+                                (if (number? baz)
+                                  (+ 1 baz)
+                                  baz)))))))
+    (test-assert (procedure? cls-0))
+    (test-group
+     "test basic object creation"
+     (let ((obj-0 (cls-0 1 2 3))
+           (obj-1 (cls-0 'a 'b 'c)))
+       (test-assert (procedure? obj-0))
+       (test-assert (procedure? obj-1))
+       (test-group
+        "test method invocation"
+        (test-equal 1 (obj-0 'quux))
+        (test-equal 4 (obj-0 'zark))
+        (test-equal 'a (obj-1 'quux))
+        (test-equal 'c (obj-1 'zark))
+        (test-group
+         "test erroneous method invocation"
+         (test-error  (obj-0 'buzz))
+         (test-error  (obj-1 'funk)))))))))
